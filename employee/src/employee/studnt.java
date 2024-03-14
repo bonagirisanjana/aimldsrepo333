b0d726853e91c4efd1e58975bfbecd3989e7a40d
online lab <online lab@DESKTOP-B840CFT>
2024-03-14 14:59:06 +0530

    push

---
+++ employee/src/employee/studnt.java
@@ -0,0 +1,143 @@
+package employee;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
+public class studnt {
+
+	private JFrame frame;
+	private JTextField tb1;
+	private JTextField tb2;
+	private JTextField tb3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					studnt window = new studnt();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public studnt() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID");
+		lblNewLabel.setForeground(new Color(255, 0, 255));
+		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 15));
+		lblNewLabel.setBounds(54, 48, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("NAME");
+		lblNewLabel_1.setForeground(new Color(255, 0, 255));
+		lblNewLabel_1.setFont(new Font("Arial Black", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(54, 98, 63, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("BRANCH");
+		lblNewLabel_2.setForeground(new Color(255, 0, 255));
+		lblNewLabel_2.setFont(new Font("Arial Black", Font.BOLD, 15));
+		lblNewLabel_2.setBounds(54, 147, 89, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("MARKS");
+		lblNewLabel_3.setForeground(new Color(255, 0, 255));
+		lblNewLabel_3.setFont(new Font("Arial Black", Font.BOLD, 15));
+		lblNewLabel_3.setBounds(54, 187, 89, 14);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		tb1 = new JTextField();
+		tb1.setBounds(186, 45, 86, 20);
+		frame.getContentPane().add(tb1);
+		tb1.setColumns(10);
+		
+		tb2 = new JTextField();
+		tb2.setBounds(186, 97, 86, 20);
+		frame.getContentPane().add(tb2);
+		tb2.setColumns(10);
+		
+		JComboBox cb1 = new JComboBox();
+		cb1.setModel(new DefaultComboBoxModel(new String[] {"CSE", "DS", "CSC", "CSIT"}));
+		cb1.setBounds(186, 145, 86, 22);
+		frame.getContentPane().add(cb1);
+		
+		tb3 = new JTextField();
+		tb3.setBounds(186, 186, 86, 20);
+		frame.getContentPane().add(tb3);
+		tb3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("SUBMIT");
+		btnNewButton.setFont(new Font("Bahnschrift", Font.BOLD, 16));
+		btnNewButton.setForeground(new Color(0, 255, 255));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id=tb1.getText();
+				String name=tb2.getText();
+				String branch=(String) cb1.getSelectedItem();
+			    String marks=tb3.getText();
+			    int m=Integer.parseInt(marks);
+			    try {
+			    	java.sql.Connection con=DriverManager.getConnection
+			    			("jdbc:mysql://localhost:3306/mydb","root","mrec");
+			    	String q="insert into students values('"+id+"','"+name+"','"+branch+"','"+m+"')";
+			    			Statement stn=con.createStatement();
+			    	        stn.executeUpdate(q);
+			    	        con.close();
+			    	        JOptionPane.showMessageDialog(btnNewButton, "inserted successs");
+			    }
+			    catch(SQLException e1)
+			    {
+			        e1.printStackTrace();	
+			    }
+			}
+		});
+		btnNewButton.setBounds(186, 227, 107, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_6 = new JLabel("STUDENT DETAILS");
+		lblNewLabel_6.setFont(new Font("Serif", Font.BOLD, 16));
+		lblNewLabel_6.setForeground(new Color(255, 0, 0));
+		lblNewLabel_6.setBounds(158, 12, 170, 14);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\online lab\\Pictures\\d3.jpg"));
+		lblNewLabel_4.setBounds(0, 0, 434, 261);
+		frame.getContentPane().add(lblNewLabel_4);
+	}
+}
